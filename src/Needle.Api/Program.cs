7ea1c30ed1147fbf16a4f1eaf4a02585a6d12c43
André Oliveira <andre.stoliver@gmail.com>
2026-06-15 14:12:39 -0300

    docs(api): add Swagger UI

--- src/Needle.Api/Program.cs
+++ src/Needle.Api/Program.cs
@@ -18,6 +18,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/openapi/v1.json",
+            "Needle API v1");
+    });
 }
 
 app.UseHttpsRedirection();
