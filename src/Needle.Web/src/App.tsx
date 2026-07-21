287755166a6958252b41a869e810c3dd1ac9bdf5
André Oliveira <andre.stoliver@gmail.com>
2026-07-21 14:34:39 -0300

    feat(web): add album search input state

--- src/Needle.Web/src/App.tsx
+++ src/Needle.Web/src/App.tsx
@@ -1,12 +1,13 @@
 import { HomeFeaturesSection } from './components/HomeFeaturesSection';
 import { HeroSection } from './components/HeroSection';
+import { AlbumSearchPreview } from './components/AlbumSearchPreview';
 import './App.css';
 
 function App() {
     return (
         <main className="app-shell">
             <HeroSection />
-
+            <AlbumSearchPreview />
             <HomeFeaturesSection />
         </main>
     );
